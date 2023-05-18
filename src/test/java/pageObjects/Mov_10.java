b13756005b1a80a859c5a21cc56a642a92b3b265
suleymandurmaz <sdurmaz77@hotmail.com>
2023-05-18 10:19:13 +0300

    18/05/2023 tum dosyalar guncellendi.

--- src/test/java/pageObjects/Mov_10.java
+++ src/test/java/pageObjects/Mov_10.java
@@ -1,43 +1,65 @@
 package pageObjects;
 
 import Base.BaseMovita;
-import Utilities.Driver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class Mov_10 extends BaseMovita {
-
-
-
 
     public static By lVerifyMap = By.xpath("//span[contains(.,'Raporlar')]");
     public static By lRaporlarSubMenu = By.xpath("//li[@class='nav-item has-sub' and contains(.,'Raporlar')]//ul//li[contains(.,'Grup Bazlı')]");
     public static By lRaporlarMenu = By.xpath("//li[@class='nav-item has-sub' and contains(.,'Raporlar')]");
     By lLoginFormSubmitButton = By.xpath("//form//button[@type='submit']");
-    By LDetaylıFiltre=By.xpath("//span[contains(.,'Detaylı Filtre')]");
+    By lDetaylıFiltre = By.xpath("//span[contains(.,'Detaylı Filtre')]");
+    public static final By lAracDropDownMenu = By.xpath("//span[@role='presentation']");
+    public static final By lStartDateDropDownMenu = By.xpath("//div[@class='card']//div[@class='form-group']");
 
     public static By loginFormİnput(String text) {
-        By lLoginForm = By.xpath("//form[@class='needs-validation mb-2 mt-10']//input[@id='" + text + "']");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(lLoginForm));
-        // WebElement element = driver.findElement(lLoginForm);
-        return lLoginForm;
+        return By.xpath("//form[@class='needs-validation mb-2 mt-10']//input[@id='" + text + "']");
+
 
     }
 
     public static By raporlarSubMenu(String text) {
-        By lRaporlarSubMenu = By.xpath("//a[contains(.,'" + text + "')]");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(lRaporlarSubMenu));
-        return lRaporlarSubMenu;
+        return  By.xpath("//a[contains(.,'" + text + "')]");
+
+
     }
-    public  static  By detailedFilterSubMenu(String text){
-        By ldetailedFilterSubMenu=By.xpath("//div[@id='user-profile']//input[@id='"+text+"']");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(ldetailedFilterSubMenu));
-        return ldetailedFilterSubMenu;
+
+    public static By detailedFilterSubMenu(String text) {
+        return  By.xpath("//div[@id='user-profile']//input[@id='" + text + "']");
+
+    }
+
+    public static By resultOfReportWith(String text) {
+        return By.xpath("//div[@class='dt-buttons']//a[contains(@title,'" + text + "')]");
+
+    }
+
+    public static By aracSelect(String text) {
+        return By.xpath("//div[@class='card']//select[@id='aracSelect']//option[contains(.,'"+text+"')]");
+
+
+    }
+    public static By selectDateMenu(String text){
+        return By.xpath("//div[@class='form-group']//input[@id='"+text+"']");
+
+    }
+    public static By selectMonth(String monthselect,String month,int num){
+        return By.xpath("(//div[@class='calendar-table']//select[@class='"+monthselect+"'])["+num+"]//option[text()='"+month+"']");
+
+    }
+    public static By selectYear(String yearselect,String year,int num){
+
+        return By.xpath("(//div[@class='calendar-table']//select[@class='"+yearselect+"'])["+num+"]//option[text()='"+year+"']");
+
+    }
+    public static By selectDay(String day,int num){
+
+        return By.xpath(" (//div[@class='calendar-table']//tr//td[text()='"+day+"'])["+num+"]");
+
+    }
     }
 
 
-}
+
